ef9c98bebd69efaaa05631138ea29868c9a52dad
Hugo Tiburcio OSF <hugotiburcio@outlook.com>
2023-01-30 21:07:23 -0300

    Search bar + Navbar fix

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -8,8 +8,8 @@ import BookmarkIcon from "../public/assets/icon-nav-bookmark.svg";
 
 const Navbar = () => {
   return (
-    <div className="absolute top-0 flex items-center justify-between w-full p-4 bg-navy-blue">
-      <Link href="/">
+    <nav className="absolute top-0 flex items-center justify-between w-full p-4 bg-navy-blue lg:flex-col lg:left-0 lg:h-full lg:w-24 lg:max-h-[60rem] lg:ml-4 lg:my-4 lg:rounded-xl">
+      <Link href="/" className="w-7 h-7 inline-block" id="hey">
         <Image
           src={"/assets/logo.svg"}
           alt="logo"
@@ -19,8 +19,11 @@ const Navbar = () => {
         />
       </Link>
 
-      <div className="flex items-center w-full justify-evenly">
-        <ul className="flex space-x-4">
+      <div
+        id="hello"
+        className="flex items-center w-full justify-evenly absolute left-0 right-0 mx-auto lg:top-20"
+      >
+        <ul className="flex space-x-4 lg:flex-col lg:justify-center lg:justify-items-center lg:space-x-0 lg:space-y-6">
           <li>
             <Link href="/">
               <HomeIcon className="text-steel-blue hover:text-white" />
@@ -43,6 +46,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+
       <Image
         src={"/assets/image-avatar.png"}
         alt="avatar"
@@ -50,7 +54,7 @@ const Navbar = () => {
         height={30}
         className="border-2 border-white rounded-full"
       />
-    </div>
+    </nav>
   );
 };
 
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,36 @@
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>
-      <>
-        <Head>
-          <title>Entertainment Web App</title>
-          <meta name="description" content="multi-page entertainment web app" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/assets/logo.svg" />
-        </Head>
+    <>
+      <Head>
+        <title>Entertainment Web App</title>
+        <meta name="description" content="multi-page entertainment web app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/logo.svg" />
+      </Head>
 
-        <main className="flex items-center justify-center min-h-screen bg-midnight-blue">
-          <Navbar/>
-          <h1 className="text-3xl font-bold text-bright-red">Hello Next</h1>
-        </main>
-      </>
-    </div>
+      <Navbar />
+      <main className="flex min-h-screen bg-midnight-blue lg:p-4">
+        {/* Search bar */}
+        <div className="flex w-full px-4 mt-24 h-8 space-x-4 lg:ml-28 lg:mt-6">
+          <Image
+            src="/assets/icon-search.svg"
+            alt="search icon"
+            width={30}
+            height={30}
+          />
+          <input
+            type="text"
+            placeholder="Search for Movies or TV Series"
+            className="outline-none w-full bg-midnight-blue text-xl text-white focus:border-b-2 focus:border-navy-blue pb-6 pt-4"
+          />
+        </div>
+      </main>
+    </>
   );
 };
 
